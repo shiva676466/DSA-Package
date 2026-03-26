7fb45f551930ad81c0861c40950fcafde6484ff6
shiva676466 <vsgvarikuppala.com>
2026-03-26 21:51:48 +0530

    updated beginner.py

--- topics/beginner.py
+++ topics/beginner.py
@@ -7,7 +7,10 @@ def beginner_topics():
         print("\nBeginner Topics: ")
         print("1. Arrays")
         print("2. Strings")
-        print("3. Back")
+        print("3. Stack")
+        print("4. Queue")
+        print("5. LinkedList")
+        print("6. Back")
         
         choice = input("Enter your choice: ")
         
@@ -16,6 +19,12 @@ def beginner_topics():
         elif choice == '2':
             strings_content()
         elif choice == '3':
+            print("Coming Soon.")
+        elif choice == '4':
+            print("Coming Soon.")
+        elif choice == '5':
+            print("Coming Soon.")
+        elif choice == '6':
             print("Exiting....!")
             break
         else:
