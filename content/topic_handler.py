93e150aafde5903cd59ac7c5e707f8bb47a01365
shiva676466 <vsgvarikuppala.com>
2026-04-16 10:41:17 +0530

    update

--- content/topic_handler.py
+++ content/topic_handler.py
@@ -1,8 +1,8 @@
 from services.content_service import get_theory, get_questions, get_code
 
-def arrays_content():
+def handle_topic(topic_name):
     while True:
-        print("\n--- Arrays ---")
+        print(f"\n--- {topic_name.capitalize()} ---")
         print("1. Theory")
         print("2. Code")
         print("3. Questions")
@@ -11,7 +11,7 @@ def arrays_content():
         choice = input("Enter choice: ")
 
         if choice == '1':
-            print("\n" + get_theory("arrays"))
+            print("\n" + get_theory(topic_name))
 
         elif choice == '2':
             print("\nSelect Language:")
@@ -22,16 +22,20 @@ def arrays_content():
             lang = input("Choose: ")
 
             if lang == '1':
-                print(get_code("arrays", "python"))
+                print(get_code(topic_name, "python"))
             elif lang == '2':
-                print(get_code("arrays", "cpp"))
+                print(get_code(topic_name, "cpp"))
             elif lang == '3':
-                print(get_code("arrays", "java"))
+                print(get_code(topic_name, "java"))
 
         elif choice == '3':
-            questions = get_questions("arrays")
+            questions = get_questions(topic_name)
+            print("\nQuestions:")
             for q in questions:
                 print("-", q)
 
         elif choice == '4':
             break
+
+        else:
+            print("Invalid choice!")
